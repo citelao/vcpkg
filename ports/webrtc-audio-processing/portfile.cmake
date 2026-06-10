621f179003b2cec5b6db14f8540b034f73b71038
Ben Stolovitz <ben@stolovitz.com>
2026-06-10 18:31:37 -0400

    PREP(webrtc-audio-processing): Add usage file, DLL cleanup, improve patch comment
    - Add usage file showing pkg_check_modules consumption pattern
    - Handle DLL bin directory for shared linkage on Windows
    - Improve abseil compat patch comment to explain removal condition

--- ports/webrtc-audio-processing/portfile.cmake
+++ ports/webrtc-audio-processing/portfile.cmake
@@ -27,4 +27,8 @@ vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static" OR NOT VCPKG_TARGET_IS_WINDOWS)
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+endif()
+
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
